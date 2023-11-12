2d17667052d5b53a6c8aac27d394f4f4cbd75bdc
taebong1012 <dksxogus1012@gmail.com>
2023-11-13 00:02:28 +0900

    FEAT: Set GlobalStyle, Theme

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    
+    body {
+        font-family: "Pretendard-Regular";
+    }
+
+`;
+
+export default GlobalStyle;
---
+++ src/style/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: "#202020",
+    primary: "#70FF00",
+    primaryOpacity: "#70FF0099",
+    textBlack: "#000000",
+    textGrey: "#666666",
+    textWhite: "#FFFFFF",
+  },
+};
+
+export default theme;
